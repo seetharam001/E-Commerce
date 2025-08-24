ca4f1ecca4b6a98b96f01a7fabb63cdde4e0847d
Seetharam46 <seetharam.p2004@gmail.com>
2025-08-24 12:28:20 +0530

    Login and Register

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
---
+++ backend/accounts/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from .models import CustomUser
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'password')
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username_or_email = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        username_or_email = data.get('username_or_email')
+        password = data.get('password')
+
+        # Allow login with username or email
+        user = authenticate(username=username_or_email, password=password)
+        if not user:
+            # try email login
+            try:
+                user_obj = CustomUser.objects.get(email=username_or_email)
+                user = authenticate(username=user_obj.username, password=password)
+            except CustomUser.DoesNotExist:
+                pass
+
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+        data['user'] = user
+        return data
