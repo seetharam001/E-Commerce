5ff4507a755f07fcae2e641f67b72ef682ef452b
Seetharam46 <seetharam.p2004@gmail.com>
2025-08-24 12:39:20 +0530

    Login and Register

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import RegisterSerializer, LoginSerializer
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+
+class LoginView(APIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        return Response({"message": "Login successful", "username": user.username, "email": user.email}, status=status.HTTP_200_OK)
